1f9456d87a6c3df6423322c5844ca25b97d3e57d
Amar Dinsa <dinsaa@gmail.com>
2024-03-04 18:27:02 -0500

    Try out Roboto condensed font (and hopefully fix unicode arrows on Android)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,17 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   theme: {
     colors: {
       dark: '#333333',
       light: '#FFFFFF',
     },
+    fontFamily: {
+      sans: [
+        'Roboto Condensed Variable',
+        ...defaultTheme.fontFamily.sans,
+      ]
+    }
   },
   content: ['./src/**/*.html'],
   plugins: [require("daisyui")]
