73e67bca5cca56c051bb9d6df1358b1807db9446
Amar Dinsa <dinsaa@gmail.com>
2024-03-04 18:41:48 -0500

    Revert "Try out Roboto condensed font (and hopefully fix unicode arrows on Android)"
    This reverts commit 1f9456d87a6c3df6423322c5844ca25b97d3e57d.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,9 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
-
 module.exports = {
   theme: {
     colors: {
       dark: '#333333',
       light: '#FFFFFF',
     },
-    fontFamily: {
-      sans: [
-        'Roboto Condensed Variable',
-        ...defaultTheme.fontFamily.sans,
-      ]
-    }
   },
   content: ['./src/**/*.html'],
   plugins: [require("daisyui")]
