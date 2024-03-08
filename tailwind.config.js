e5101241d570eee87acbf38b2e97740cd03127b4
Sawyer Powell <sawyerhpowell@gmail.com>
2024-03-07 19:44:43 -0800

    Updated nav bar, font size on content pages, and arrow buttons on index page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,18 @@
 module.exports = {
   theme: {
+    fontFamily: {
+	sans: ['Noto Sans SC', 'sans-serif'],
+	serif: ['Noto Serif','serif'],
+    },
     colors: {
-      dark: '#333333',
-      light: '#FFFFFF',
+	dark: '#333333',
+	light: '#FFFFFF',
+	gray: '#F5F5F5',
     },
   },
   content: ['./src/**/*.html'],
-  plugins: [require("daisyui"), require("@tailwindcss/forms")]
+    plugins: [
+	require("daisyui"),
+	require("@tailwindcss/forms"),
+    ]
 }
